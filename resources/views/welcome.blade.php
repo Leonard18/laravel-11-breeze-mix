9a307e553cb5f6ce0f2c1e1351f9f49cee37b2ba
Leonard Ojojo <32959851+Leonard18@users.noreply.github.com>
2024-06-24 23:46:35 +0100

    Fixed an issues with the mix-manifest.json file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body class="antialiased font-sans">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
@@ -141,5 +141,6 @@
                 </div>
             </div>
         </div>
+        <script src="{{ mix('js/app.js') }}" defer></script>
     </body>
 </html>
